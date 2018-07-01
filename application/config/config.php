4177b57ad816649ad0bffaf3a64037f2ba706fd0
Inggrit Siswati Putri <inggritputri8@gmail.com>
2018-07-01 14:38:38 +0700

    update route name

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost';//'https://crud-app-ci.herokuapp.com/';
+$config['base_url'] = 'https://crud-ci-heroku.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
